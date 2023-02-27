bba564a44c88b5699e3d43a5819fc1097d7c80d3
Callum Jones <emailcallumj@gmail.com>
2023-02-27 11:55:47 +1100

    Update server to work with refactored backend

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,6 @@ from flask import Flask, request
 
 from auth import AuthHandler
 from database import Database
-
 from user import User
 
 
@@ -20,14 +19,14 @@ auth_manager = AuthHandler(db)
 def register() -> Dict:
     register_details: Optional[dict] = request.json
     if register_details is None:
-        return {"error": "pray"}
+        return {"error": "request_json_error"}, 400
 
     if "username" not in register_details:
-        return {"error": "missing_username_field"}
+        return {"error": "missing_username_field"}, 400
     if "password" not in register_details:
-        return {"error": "missing_password_field"}
+        return {"error": "missing_password_field"}, 400
     if "email" not in register_details:
-        return {"error": "missing_email_field"}
+        return {"error": "missing_email_field"}, 400
 
     auth_res = auth_manager.register_user(register_details["username"],
                        register_details["password"], register_details["email"])
@@ -38,13 +37,13 @@ def register() -> Dict:
 def login() -> Dict:
     login_details: Optional[dict] = request.json
     if login_details is None:
-        return {"error": "pray"}
+        return {"error": "request_json_error"}, 400
 
     if "username" not in login_details:
-        return {"error": "missing_username_field"}
+        return {"error": "missing_username_field"}, 400
 
     if "password" not in login_details:
-        return {"error": "missing_pwd_field"}
+        return {"error": "missing_pwd_field"}, 400
 
     login_res = auth_manager.login_user(login_details["username"], login_details["password"])
     return login_res
@@ -53,52 +52,57 @@ def login() -> Dict:
 def logout() -> Dict:
     logout_details: Optional[dict] = request.json
     if logout_details is None:
-        return {"error": "pray"}
+        return {"error": "request_json_error"}, 400
 
     if "token" not in logout_details:
-        return {"error": "missing_token"}
+        return {"error": "missing_token"}, 400
 
     logout_res = auth_manager.logout_user(logout_details["token"])
     return logout_res
 
-# @app.route("/user/<username>/get_reviews")
-# def get_reviews(username: str) -> Dict:
-#     target_usr = database.find_user_by_name(username)
-#     if target_usr is None:
-#         return {"error": "user_does_not_exist"}
+@app.route("/user/<username>/get_reviews")
+def get_reviews(username: str) -> Dict:
+    target_usr = db.get_user_by_name(username)
+    if target_usr is None:
+        return {"error": "user_does_not_exist"}
 
-#     reviews = target_usr.fetchReviewsAsDict()
-#     return {"user": username, "reviews": reviews}
+    reviews = target_usr.fetchReviewsAsDict()
+    print(target_usr)
+    print(reviews)
+    return {"user": username, "reviews": reviews}
 
 
-# @app.route("/user/add_review", methods=["POST"])
-# def add_review():
-#     add_review_details: Optional[dict] = request.json
-#     if add_review_details is None:
-#         return {"error": "pray"}, 400
+@app.route("/user/add_review", methods=["POST"])
+def add_review():
+    add_review_details: Optional[dict] = request.json
+    if add_review_details is None:
+        return {"error": "request_json_error"}, 400
 
-#     if "token" not in add_review_details:
-#         return {"status": "error", "code": "missing_token"}, 400
+    if "token" not in add_review_details:
+        return {"status": "error", "code": "missing_token"}, 400
 
-#     if "game_id" not in add_review_details:
-#         return {"status": "error", "code": "missing_game_id"}, 400
+    if "game_id" not in add_review_details:
+        return {"status": "error", "code": "missing_game_id"}, 400
 
-#     if "rating" not in add_review_details:
-#         return {"status": "error", "code": "missing_rating"}, 400
+    if "rating" not in add_review_details:
+        return {"status": "error", "code": "missing_rating"}, 400
 
-#     if "comment" not in add_review_details:
-#         return {"status": "error", "code": "missing_comment"}, 400
+    if "comment" not in add_review_details:
+        return {"status": "error", "code": "missing_comment"}, 400
 
-#     user: Optional[User] = database.lookup_token_user(add_review_details["token"])
-#     if user is None:
-#         return {"status": "error", "code": "invalid_token"}, 400
+    user: Optional[User] = db.get_usr_by_token(add_review_details["token"])
+    if user is None:
+        return {"status": "error", "code": "invalid_token"}, 400
 
-#     response = user.addReview(add_review_details["game_id"],
-#                               add_review_details["rating"],
-#                               add_review_details["comment"])
+    print("DEBUG: " + str(user))
+    response = user.addReview(int(add_review_details["game_id"]),
+                              add_review_details["rating"],
+                              add_review_details["comment"])
+    print(db.users_cache)
+    print(user)
+    db.update_from_caches()
 
-#     if response["status"] == "error":
-#         return response, 400
-#     else:
-#         return response
-
+    if response["status"] == "error":
+        return response, 400
+    else:
+        return response
